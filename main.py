dd1bdda7ca82a014ebc4dd00aadcfc1d5c8bfeea
flubshi <flubshi@web.de>
2019-01-13 00:55:47 +0100

    Implement basic recording support (playback of recorded items) release 0.0.8

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ password = xbmcplugin.getSetting(_handle, "password")
 # open settings, 
 if not username or not password:
     xbmcaddon.Addon().openSettings()
+    # TODO not working as expected
     username = xbmcplugin.getSetting(_handle, "username")
     password = xbmcplugin.getSetting(_handle, "password")
 
@@ -41,6 +42,61 @@ def get_url(**kwargs):
     :rtype: str
     """
     return '{0}?{1}'.format(_url, urlencode(kwargs))
+
+
+def get_default():
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, 'waipu.tv')
+
+    # TV channel list
+    list_item = xbmcgui.ListItem(label="Live TV")
+    url = get_url(action='list-channels')
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, isFolder=True)
+
+    # recordings list
+    list_item = xbmcgui.ListItem(label="Aufnahmen")
+    url = get_url(action='list-recordings')
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, isFolder=True)
+
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_recordings():
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, 'waipu.tv')
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get video categories
+    try:
+        recordings = w.getRecordings()
+    except Exception as e:
+        dialog = xbmcgui.Dialog().ok("Error", str(e))
+        return
+    # Iterate through categories
+    for recording in recordings:
+        list_item = xbmcgui.ListItem(label=recording['epgData']['title'])
+        list_item.setInfo('video', {'title': recording['epgData']['title'],
+                                    'genre': recording['epgData']['genre'],
+                                    #'tracknumber': channel_data['orderIndex'] + 1,
+                                    # 'plot': "Langinfo",
+                                    # 'tagline': "Tagline",
+                                    'mediatype': 'video'})
+        for previewImage in recording['epgData']['previewImages']:
+            previewImage += "?width=200&height=200"
+            xbmc.log("waipu image: " + previewImage, level=xbmc.LOGDEBUG)
+            list_item.setArt(
+            {'thumb': previewImage, 'icon': previewImage, 'clearlogo': previewImage})
+            break
+        list_item.setProperty('IsPlayable', 'true')
+        url = get_url(action='play-recording', recordingid=recording["id"])
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, isFolder=False)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
 
 def list_channels():
     # Set plugin category. It is displayed in some skins as the name
@@ -65,9 +121,8 @@ def list_channels():
                                     #'tagline': "Tagline",
                                     'mediatype': 'video'})
         list_item.setArt({'thumb': channel_data['icon'], 'icon': channel_data['icon'], 'clearlogo': channel_data['icon']})
-        list_item.setLabel2('Casino Royale')
         list_item.setProperty('IsPlayable', 'true')
-        url = get_url(action='play', channel=channel)
+        url = get_url(action='play-channel', channel=channel)
         xbmcplugin.addDirectoryItem(_handle, url, list_item, isFolder = False)
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_TRACKNUM)
@@ -96,7 +151,7 @@ def play_channel(channel):
                 if path:
                     path=path+"|User-Agent="+user_agent
                     print path
-                    break;
+                    break
 
     listitem = xbmcgui.ListItem(channel, path=path)
     listitem.setMimeType('application/xml+dash')
@@ -115,21 +170,68 @@ def play_channel(channel):
     xbmc.log("waipu license: "+license_str, level=xbmc.LOGDEBUG)
     listitem.setProperty(is_helper.inputstream_addon + '.license_key', "https://drm.wpstr.tv/license-proxy-widevine/cenc/|User-Agent="+user_agent+"&Content-Type=text%2Fxml&x-dt-custom-data="+license_str+"|R{SSM}|JBlicense")
 
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=listitem)
+
+def play_recording(recordingid):
+
+    is_helper = inputstreamhelper.Helper('mpd', drm='widevine')
+    if not is_helper.check_inputstream():
+        return False
+
+    user_agent = "waipu-2.29.3-370e0a4-9452 (Android 8.1.0)"
+    print "Playing "+recordingid
+    """
+    Play a video by the provided path.
+
+    :param path: Fully-qualified video URL
+    :type path: str
+    """
+    streamingData = w.playRecording(recordingid)
+    for stream in streamingData["streamingDetails"]["streams"]:
+        if (stream["protocol"] == 'MPEG_DASH'):
+                path=stream["href"]
+                if path:
+                    path=path+"|User-Agent="+user_agent
+                    print path
+                    break
+
+    listitem = xbmcgui.ListItem(streamingData["epgData"]["title"], path=path)
+    listitem.setMimeType('application/xml+dash')
+    listitem.setProperty(is_helper.inputstream_addon + ".license_type", "com.widevine.alpha")
+    listitem.setProperty(is_helper.inputstream_addon + ".manifest_type", "mpd")
+    listitem.setProperty('inputstreamaddon', is_helper.inputstream_addon)
+
+    # Prepare for drm keys
+    jwtheader,jwtpayload,jwtsignature = w.getToken().split(".")
+    xbmc.log("waipu jwt payload: "+jwtpayload, level=xbmc.LOGDEBUG)
+    jwtpayload_decoded = base64.b64decode(jwtpayload + '=' * (-len(jwtpayload) % 4))
+    jwt_json = json.loads(jwtpayload_decoded)
+    xbmc.log("waipu userhandle: "+jwt_json["userHandle"], level=xbmc.LOGDEBUG)
+    license = {'merchant' : 'exaring', 'sessionId' : 'default', 'userId' : jwt_json["userHandle"]}
+    license_str=base64.b64encode(json.dumps(license))
+    xbmc.log("waipu license: "+license_str, level=xbmc.LOGDEBUG)
+    listitem.setProperty(is_helper.inputstream_addon + '.license_key', "https://drm.wpstr.tv/license-proxy-widevine/cenc/|User-Agent="+user_agent+"&Content-Type=text%2Fxml&x-dt-custom-data="+license_str+"|R{SSM}|JBlicense")
 
     xbmcplugin.setResolvedUrl(_handle, True, listitem=listitem)
 
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
     if params:
-        if params['action'] == 'play':
+        if params['action'] == "play-channel":
             play_channel(params['channel'])
+        elif params['action'] == "list-channels":
+            list_channels()
+        elif params['action'] == "list-recordings":
+            list_recordings()
+        elif params['action'] == "play-recording":
+            play_recording(params['recordingid'])
         else:
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
             # e.g. typos in action names.
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
-        list_channels()
+        get_default()
 
 
 if __name__ == '__main__':
--- waipu.py
+++ waipu.py
@@ -3,89 +3,114 @@ from pprint import pprint
 import requests
 import time
 
+
 class Waipu:
-	def __init__(self, username, password):
-		self._auth = None
-		self.logged_in = False
-		self._channelData = None
-		self.__username = username
-		self.__password = password
+    def __init__(self, username, password):
+        self._auth = None
+        self.logged_in = False
+        self._channelData = None
+        self.__username = username
+        self.__password = password
 
-	def fetchToken(self):
-		url  = "https://auth.waipu.tv/oauth/token"
-		payload = {'username': self.__username, 'password': self.__password, 'grant_type': 'password'}
-		headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)', 'Authorization': 'Basic YW5kcm9pZENsaWVudDpzdXBlclNlY3JldA=='}
-		self._auth = None
-		self.logged_in = False
-		r = requests.post(url, data=payload, headers=headers)
-		if r.status_code == 200:
-			self._auth = r.json()
-			self.logged_in = True
-			self._auth["expires"] = time.time()+self._auth["expires_in"]
-		return r.status_code
+    def fetchToken(self):
+        url = "https://auth.waipu.tv/oauth/token"
+        payload = {'username': self.__username, 'password': self.__password, 'grant_type': 'password'}
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                   'Authorization': 'Basic YW5kcm9pZENsaWVudDpzdXBlclNlY3JldA=='}
+        self._auth = None
+        self.logged_in = False
+        r = requests.post(url, data=payload, headers=headers)
+        if r.status_code == 200:
+            self._auth = r.json()
+            self.logged_in = True
+            self._auth["expires"] = time.time() + self._auth["expires_in"]
+        return r.status_code
 
-	def getToken(self):
-		if (self._auth == None or self._auth["expires"] <= time.time()):
-			code = self.fetchToken()
-			if (code != 200):
-				raise Exception("Can't login, Code="+str(code))
-		#TODO: renew token
-		#print(self._auth)
-		return self._auth['access_token']
-	
-	def getChannels(self):
-		self.getToken()
-		url  = "https://epg.waipu.tv/api/channels"
-		headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)', 'Authorization': 'Bearer '+self._auth['access_token']}
-		r = requests.get(url, headers=headers)
-		if r.status_code == 200:		
-			self._channelData = r.json()
-			#pprint (self._channelData)
-			channels = {}
-			for channel in  self._channelData:
-				channel_data = {}
-				for link in channel['links']:
-					if (link['rel'] == 'liveImage'):
-						channel_data['thumbnail'] = link['href']
-					if (link['rel'] == 'icon'):
-						channel_data['icon'] = link['href']+"?width=1000&height=1000"
-				channel_data['displayName'] = channel['displayName']
-				channel_data['orderIndex'] = channel['orderIndex']
-				channels[channel['id']] = channel_data
-			return channels
+    def getToken(self):
+        if (self._auth == None or self._auth["expires"] <= time.time()):
+            code = self.fetchToken()
+            if (code != 200):
+                raise Exception("Can't login, Code=" + str(code))
+        # TODO: renew token
+        # print(self._auth)
+        return self._auth['access_token']
 
-	def getStatus(self):
-		self.getToken()
-		url  = "https://status.wpstr.tv/status?nw=wifi"
-		headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)'}
-		r = requests.get(url, headers=headers)
-		#pprint(r.json())
-		if r.status_code == 200:		
-			pprint(r.json())
+    def getChannels(self):
+        self.getToken()
+        url = "https://epg.waipu.tv/api/channels"
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                   'Authorization': 'Bearer ' + self._auth['access_token']}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            self._channelData = r.json()
+            # pprint (self._channelData)
+            channels = {}
+            for channel in self._channelData:
+                channel_data = {}
+                for link in channel['links']:
+                    if (link['rel'] == 'liveImage'):
+                        channel_data['thumbnail'] = link['href']
+                    if (link['rel'] == 'icon'):
+                        channel_data['icon'] = link['href'] + "?width=1000&height=1000"
+                channel_data['displayName'] = channel['displayName']
+                channel_data['orderIndex'] = channel['orderIndex']
+                channels[channel['id']] = channel_data
+            return channels
 
-	def homeCheck(self, mac="66:09:80:70:c4:75"):
-		self.getToken()
-		url  = "https://home-check.waipu.tv/api/users/home-networks/"+mac
-		headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)', 'Authorization': 'Bearer '+self._auth['access_token']}
-		
-		r = requests.get(url, headers=headers)
-		#pprint(r.json())
-		if r.status_code == 200:		
-			pprint(r.json())
+    def getRecordings(self):
+        self.getToken()
+        url = "https://recording.waipu.tv/api/recordings"
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                   'Authorization': 'Bearer ' + self._auth['access_token']}
+        r = requests.get(url, headers=headers)
+        recordings = []
+        if r.status_code == 200:
+            for recording in r.json():
+                if recording['status'] == "FINISHED":
+                    recordings.append(recording)
+        return recordings
 
-	def playChannel(self, id):
-		if (self._channelData == None):
-			self.getChannels()
-		self.getStatus()
-		#self.homeCheck()
-		for channel in  self._channelData:
-			if (channel['id'] == id):
-				for link in channel['links']:
-					if (link['rel'] == 'livePlayout'):
-						url =  link['href']
-						payload = {'network': 'wlan'}
-						headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)', 'Authorization': 'Bearer '+self._auth['access_token']}
-						r = requests.get(url, data=payload, headers=headers)
-						return r.json()
+    def getStatus(self):
+        self.getToken()
+        url = "https://status.wpstr.tv/status?nw=wifi"
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)'}
+        r = requests.get(url, headers=headers)
+        # pprint(r.json())
+        if r.status_code == 200:
+            pprint(r.json())
 
+    def homeCheck(self, mac="66:09:80:70:c4:75"):
+        self.getToken()
+        url = "https://home-check.waipu.tv/api/users/home-networks/" + mac
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                   'Authorization': 'Bearer ' + self._auth['access_token']}
 
+        r = requests.get(url, headers=headers)
+        # pprint(r.json())
+        if r.status_code == 200:
+            pprint(r.json())
+
+    def playChannel(self, id):
+        if (self._channelData == None):
+            self.getChannels()
+        self.getStatus()
+        # self.homeCheck()
+        for channel in self._channelData:
+            if (channel['id'] == id):
+                for link in channel['links']:
+                    if (link['rel'] == 'livePlayout'):
+                        url = link['href']
+                        payload = {'network': 'wlan'}
+                        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                                   'Authorization': 'Bearer ' + self._auth['access_token']}
+                        r = requests.get(url, data=payload, headers=headers)
+                        return r.json()
+
+    def playRecording(self, id):
+        self.getToken()
+        url = "https://recording.waipu.tv/api/recordings/" + str(id)
+        payload = {'network': 'wlan'}
+        headers = {'User-Agent': 'waipu-2.29.2-c0f220b-9446 (Android 8.1.0)',
+                   'Authorization': 'Bearer ' + self._auth['access_token']}
+        r = requests.get(url, data=payload, headers=headers)
+        return r.json()
